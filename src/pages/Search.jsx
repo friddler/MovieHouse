9423bf29591327007d571ecf878811e832f5e762
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-10-18 10:56:54 +0200

    now able to search for both movies and series in search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import '../styles/Search.css';
-import { Link } from 'react-router-dom'; 
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../styles/Search.css";
+import { Link } from "react-router-dom";
 
 const Search = (props) => {
-  const [movieData, setMovieData] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const apiKey = '6e8f505c75786ea1c4d7fa68159ede64';
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const apiKey = "6e8f505c75786ea1c4d7fa68159ede64";
 
   useEffect(() => {
-    const fetchMovies = async () => {
+    const fetchMedia = async () => {
       try {
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=${apiKey}&query=${searchTerm}`);
+        const movieResponse = axios.get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`
+        );
+        const seriesResponse = axios.get(
+          `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${searchTerm}`
+        );
 
-        if (response.status !== 200) {
-          throw new Error('Något gick fel vid hämtning av filmer');
-        }
+        const responses = await Promise.all([movieResponse, seriesResponse]);
+        const combinedData = [
+          ...responses[0].data.results,
+          ...responses[1].data.results,
+        ];
 
-        const data = response.data;
-
-        if (data && data.results && Array.isArray(data.results)) {
-          setMovieData(data.results);
-        } else {
-          console.error('API Response is not as expected:', data);
-        }
+        setData(combinedData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    if (searchTerm !== '') {
-      fetchMovies();
+    if (searchTerm !== "") {
+      fetchMedia();
     }
   }, [searchTerm, apiKey]);
 
@@ -44,39 +44,36 @@ const Search = (props) => {
       <h3 id="utforska">Utforska</h3>
       <input
         type="text"
-        placeholder="Sök efter filmer..."
+        placeholder="Sök efter filmer och serier..."
         value={searchTerm}
         onChange={handleSearch}
         className="search-input"
       />
       <div className="search-list">
-        {movieData.map((movie, index) => (
-          <div key={index} className="movie-card">
-            <Link to={`/movieinfo/${movie.id}`}>
-            <img
-              src={movie.poster_path
-                ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-                : "image.png" 
-                
-              }
-              alt={movie.title}
-              className={movie.poster_path ? "movie-image" : "default-image"}
-
-              onError={(e) => {
-                e.target.style.display = 'none'; 
-              }}
-            />
+        {data.map((item, index) => (
+          <div key={index} className="media-card">
+            <Link to={`/${item.title ? "movieinfo" : "seriesinfo"}/${item.id}`}>
+              <img
+                src={
+                  item.poster_path
+                    ? `https://image.tmdb.org/t/p/w300${item.poster_path}`
+                    : "image.png"
+                }
+                alt={item.title || item.name}
+                className={item.poster_path ? "media-image" : "default-image"}
+                onError={(e) => {
+                  e.target.style.display = "none";
+                }}
+              />
             </Link>
-            {(!movie.poster_path && movie.title) && (
-              <div className="movie-title">{movie.title}</div>
+            {!item.poster_path && (item.title || item.name) && (
+              <div className="media-title">{item.title || item.name}</div>
             )}
           </div>
         ))}
       </div>
     </div>
   );
-  
-
 };
 
 export default Search;
